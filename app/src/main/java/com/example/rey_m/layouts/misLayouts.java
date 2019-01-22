dae4a3a88cb7d13b44d9e8ca1c1b2bea41b2ca14
kiikedls <jespinozadelossatnos@gmail.com>
2019-01-21 19:16:45 -0600

    Initial commit

---
+++ app/src/main/java/com/example/rey_m/layouts/misLayouts.java
@@ -0,0 +1,13 @@
+package com.example.rey_m.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class misLayouts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mis_layouts);
+    }
+}
